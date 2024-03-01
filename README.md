bb3fa740e4383bb7ab49ff178b36aa7c691fb8e2
Jackson Frey <jacksonf955@gmail.com>
2024-02-29 21:35:11 -0500

    demo for meeting

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CineMatrix
 Group Project for CSE201 
+
+# How to use
+1. Download NodeJS
+Go the the [NodeJS website](https://nodejs.org/en/download) and download the executable for 
+your operating system. <br>
+Note: it will ask if you want to install something called chocolatey. This isn't required for this project so don't install it (unless you want to I guess).
+
+2. Download Github Repo
+Either download the .zip or type 
+```
+git clone https://github.com/Jack-Frey/CineMatrix 
+```
+into your terminal, whatever is easier.
+
+3. Install dependencies
+Navigate to the project folder and type
+```
+npm install
+```
+to download the packages needed to run the project
+
+4. Run it
+To start the server, type
+```
+node index.js
+```
+If everything works, you'll see the text
+```
+App listening on port 3000
+```
+Finally, open a web browser and type `localhost:3000` into the address bar.
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser') 
+
+const port = 3000
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+//app.use(express.json())
+//app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/search/movie', (req, res) => {
+    //console.log(req.body);
+    var movieName = req.body.textbox;
+    var url = 'https://api.themoviedb.org/3/search/movie?query=' 
+        + movieName + '&include_adult=false&language=en-US&page=1';
+    var options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjAyMGE3NDdlOThkYWFkZTk3YmZhNGJjMGJmYzI3MiIsInN1YiI6IjY1ZTBmNzliYTM5ZDBiMDE2MzA3ZDhmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w7gWjNh2OHWhiq5Uh_JhhKG78c2ktBEimG8Vm89REXo'
+        }
+    }
+
+    fetch(url, options)
+        .then(data => data.json())
+        .then(data => {
+            var movies = data["results"]
+            var count = Object.keys(movies).length
+            var names = ""
+            for (let i = 0; i < count; i++) {
+                let name = movies[i]['title']
+                let release = movies[i]['release_date'].slice(0, 4)
+                names += (name + " (" + release + ") <br>") 
+            }
+            res.send(names)
+        })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
