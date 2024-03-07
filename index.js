2aa6e38e1d2eab415b270644d0102c4901ca4fca
JacksonGuy <jacksonf955@gmail.com>
2024-03-07 15:07:37 -0500

    TV Show search; combines with movie search

--- index.js
+++ index.js
@@ -15,10 +15,40 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.post('/search/movie', (req, res) => {
-    var movieName = req.body.textbox;
-    var url = 'https://api.themoviedb.org/3/search/movie?query=' 
-        + movieName + '&include_adult=false&language=en-US&page=1';
+async function get_results(name, options) {
+    var urlMovies = 'https://api.themoviedb.org/3/search/movie?query='
+        + name + '&include_adult=false&language=en-US&page=1'
+    var urlShows = 'https://api.themoviedb.org/3/search/tv?query='
+        + name + '&include_adult=false&language=en-US&page=1'    
+
+    var results = []
+
+    const [response1, response2] = await Promise.all([
+        fetch(urlMovies, options),
+        fetch(urlShows, options)
+    ])
+
+    const movieResults = await response1.json()
+    const TVResults = await response2.json()
+
+    for (let index in movieResults['results']) {
+        results.push(movieResults['results'][index])
+    }
+    for (let index in TVResults['results']) {
+        results.push(TVResults['results'][index])
+    }
+
+    return results
+}
+
+app.post('/search', (req, res) => {
+    var contentName = req.body.textbox;
+
+    var urlMovies = 'https://api.themoviedb.org/3/search/movie?query='
+        + contentName + '&include_adult=false&language=en-US&page=1'
+    var urlShows = 'https://api.themoviedb.org/3/search/tv?query='
+        + contentName + '&include_adult=false&language=en-US&page=1'
+
     var options = {
         method: 'GET',
         headers: {
@@ -27,19 +57,23 @@ app.post('/search/movie', (req, res) => {
         }
     }
 
-    fetch(url, options)
-        .then(data => data.json())
-        .then(data => {
-            var movies = data["results"]
-            var count = Object.keys(movies).length
-            var names = ""
-            for (let i = 0; i < count; i++) {
-                let name = movies[i]['title']
-                let release = movies[i]['release_date'].slice(0, 4)
-                names += (name + " (" + release + ") <br>") 
+    get_results(contentName, options).then(results => {
+        var display = ""
+        for (let i = 0; i < results.length; i++) {
+            if ("title" in results[i]) {
+                let name = results[i]['title']
+                let date = results[i]['release_date'].slice(0, 4)
+                let str = name + " (" + date + ") <br>"
+                display += str
+            } else {
+                let name = results[i]['name'];
+                let date = results[i]['first_air_date'].slice(0,4)
+                let str = name +" (" + date + ") <br>"
+                display += str
             }
-            res.send(names)
-        })
+        }
+        res.send(display)
+    })
 })
 
 app.listen(port, () => {
